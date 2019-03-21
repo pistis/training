6cb2be24a11c65c0150b3fd49da28dc8b0e1876e
pistis <vamalboro@gmail.com>
2019-03-21 10:44:09 +0900

    solved two sum

---
+++ leetcode/easy/twosum.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/two-sum/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+}
+
+console.assert(twoSum([2, 7, 11, 15], 9).toString() === [0, 1].toString())
