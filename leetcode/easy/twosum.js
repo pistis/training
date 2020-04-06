0f4fa1dc7eb81cbf22554edcf47171fddb1fc752
pistis <vamalboro@gmail.com>
2020-04-07 01:21:45 +0900

    solved three sum, but time limit... when run test cases 313...

--- leetcode/easy/twosum.js
+++ leetcode/easy/twosum.js
@@ -5,14 +5,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
-        }
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-}
+  }
+};
 
-console.assert(twoSum([2, 7, 11, 15], 9).toString() === [0, 1].toString())
+console.assert(twoSum([2, 7, 11, 15], 9).toString() === [0, 1].toString());
+
+// solution
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], i);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement) && map.get(complement) !== i) {
+      return [i, map.get(complement)];
+    }
+  }
+  throw new Error("No two sum solution");
+};
