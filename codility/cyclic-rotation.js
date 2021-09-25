65ce4cd590418e9a406ea1815c562afea50224d9
pistis <vamalboro@gmail.com>
2021-09-25 15:06:56 +0900

    solve cyclic rotation array - codility

---
+++ codility/cyclic-rotation.js
@@ -0,0 +1,18 @@
+function solution(A, K) {
+    if (K === 0 || K === A.length) return A;
+
+    const pivot = K > A.length ? A.length - (K % A.length) : A.length - K;
+    return A.slice(pivot).concat(A.slice(0, pivot));
+}
+
+solution([3, 8, 9, 7, 6], 0);
+solution([3, 8, 9, 7, 6], 1);
+solution([3, 8, 9, 7, 6], 2);
+solution([3, 8, 9, 7, 6], 3);
+solution([3, 8, 9, 7, 6], 4);
+solution([3, 8, 9, 7, 6], 5);
+solution([3, 8, 9, 7, 6], 6);
+
+
+0 1 2 3 4 5 6 7 8 9
+1 2 3 4 5 6 7 8 9 10
